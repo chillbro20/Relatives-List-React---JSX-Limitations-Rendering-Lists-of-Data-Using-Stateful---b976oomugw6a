344242a0632c20fdf280b6001dffbfff3f5ba193
chillbro20 <97622677+chillbro20@users.noreply.github.com>
2022-10-01 23:46:23 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,17 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    render() {
 
+    render() {
         return(
             <div id="main">
                {/* Do not remove the main div */}
+               <ol key="relativeList">
+                <li key="relativeListItem1">Saurabh</li>
+                <li key="relativeListItem2">Durgesh</li>
+                <li key="relativeListItem3">Jaya</li>
+                <li key="relativeListItem4">Sakashi</li>
+               </ol>
             </div>
         )
     }
